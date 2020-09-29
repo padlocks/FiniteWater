4bd77ed2efbe96970ce9d18871b9fe19fffb041b
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2020-09-29 10:20:23 -0400

    port to 1.16.3

--- src/main/java/tfar/finitewater/Config.java
+++ src/main/java/tfar/finitewater/Config.java
@@ -23,37 +23,47 @@ public class Config {
 		public static ForgeConfigSpec.ConfigValue<List<? extends String>> infinite_water_biomes;
 		public static ForgeConfigSpec.ConfigValue<List<? extends String>> infinite_lava_biomes;
 
+		public static ForgeConfigSpec.BooleanValue is_water_whitelist;
+		public static ForgeConfigSpec.BooleanValue is_lava_whitelist;
+
+
 		ServerConfig(ForgeConfigSpec.Builder builder) {
 			builder.push("general");
-
-			infinite_water_biomes = builder
-							.comment("Biomes that water is infinite in")
-							.translation("text.finitewater.config.is_water_finite")
-							.defineList("infinite water biomes", Lists.newArrayList(
-											Biomes.COLD_OCEAN.getRegistryName().toString(),
-											Biomes.DEEP_COLD_OCEAN.getRegistryName().toString(),
-											Biomes.DEEP_FROZEN_OCEAN.getRegistryName().toString(),
-											Biomes.DEEP_LUKEWARM_OCEAN.getRegistryName().toString(),
-											Biomes.DEEP_OCEAN.getRegistryName().toString(),
-											Biomes.DEEP_WARM_OCEAN.getRegistryName().toString(),
-											Biomes.FROZEN_OCEAN.getRegistryName().toString(),
-											Biomes.FROZEN_RIVER.getRegistryName().toString(),
-											Biomes.OCEAN.getRegistryName().toString(),
-											Biomes.RIVER.getRegistryName().toString(),
-											Biomes.WARM_OCEAN.getRegistryName().toString()
+			builder.push("lava");
+			is_lava_whitelist = builder.comment("Is the list below a whitelist, changing to false will make it a blacklist instead").define("whitelist",true);
+			infinite_lava_biomes = builder
+					.comment("Biomes that lava is infinite in")
+					.translation("text.finitewater.config.is_lava_infinite")
+					.defineList("infinite lava biomes",
+							Lists.newArrayList(
+									Biomes.CRIMSON_FOREST.getLocation().toString(),
+									Biomes.NETHER_WASTES.getLocation().toString(),
+									Biomes.SOUL_SAND_VALLEY.getLocation().toString(),
+									Biomes.WARPED_FOREST.getLocation().toString()
 							), String.class::isInstance);
 
-			infinite_lava_biomes = builder
-							.comment("Biomes that lava is infinite in")
-							.translation("text.finitewater.config.is_lava_infinite")
-							.defineList("infinite lava biomes",
-											Lists.newArrayList(
-															Biomes.CRIMSON_FOREST.getRegistryName().toString(),
-															Biomes.NETHER_WASTES.getRegistryName().toString(),
-															Biomes.SOUL_SAND_VALLEY.getRegistryName().toString(),
-															Biomes.WARPED_FOREST.getRegistryName().toString()
-											), String.class::isInstance);
+
 			builder.pop();
+			builder.push("water");
+			is_water_whitelist = builder.comment("Is the list below a whitelist, changing to false will make it a blacklist instead").define("whitelist",true);
+
+			infinite_water_biomes = builder
+					.comment("Biomes that water is infinite in")
+					.translation("text.finitewater.config.is_water_finite")
+					.defineList("infinite water biomes", Lists.newArrayList(
+							Biomes.COLD_OCEAN.getLocation().toString(),
+							Biomes.DEEP_COLD_OCEAN.getLocation().toString(),
+							Biomes.DEEP_FROZEN_OCEAN.getLocation().toString(),
+							Biomes.DEEP_LUKEWARM_OCEAN.getLocation().toString(),
+							Biomes.DEEP_OCEAN.getLocation().toString(),
+							Biomes.DEEP_WARM_OCEAN.getLocation().toString(),
+							Biomes.FROZEN_OCEAN.getLocation().toString(),
+							Biomes.FROZEN_RIVER.getLocation().toString(),
+							Biomes.OCEAN.getLocation().toString(),
+							Biomes.RIVER.getLocation().toString(),
+							Biomes.WARM_OCEAN.getLocation().toString()
+					), String.class::isInstance);
+			builder.pop(2);
 		}
 	}
 }
